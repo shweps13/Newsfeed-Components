acedbda463ec752e3a0f903ffdf909d545fc5b68
Heorhii Siburov <g.siburov@gmail.com>
2019-08-07 15:58:22 -0700

    menu is done

--- Article/Article.js
+++ Article/Article.js
@@ -116,7 +116,27 @@ const data = [
     fourthParagraph: `Hodor hodor - hodor... Hodor hodor hodor hodor. Hodor. Hodor! Hodor hodor, hodor hodor hodor hodor hodor; hodor hodor? Hodor!
           Hodor hodor, HODOR hodor, hodor hodor?! Hodor! Hodor hodor, HODOR hodor, hodor hodor, hodor, hodor hodor. Hodor, hodor.
           Hodor. Hodor, hodor, hodor. Hodor hodor... Hodor hodor hodor?! Hodor, hodor... Hodor hodor HODOR hodor, hodor hodor. Hodor.`
+  },
+  {
+    title: 'Professional Software Development in 2021',
+    date: 'Jan 1st, 2020',
+    firstParagraph: `Hodor hodor HODOR! Hodor hodor - hodor, hodor. Hodor hodor... Hodor hodor hodor; hodor hodor. Hodor hodor hodor, hodor, hodor
+          hodor. Hodor, hodor. Hodor. Hodor, hodor - hodor... Hodor hodor hodor; hodor HODOR hodor, hodor hodor?! Hodor hodor, hodor.
+          Hodor hodor hodor hodor hodor! Hodor hodor - HODOR hodor, hodor hodor hodor hodor hodor; hodor hodor? `,
+
+    secondParagraph: `Hodor, hodor. Hodor. Hodor, hodor, hodor. Hodor hodor, hodor. Hodor hodor, hodor, hodor hodor. Hodor! Hodor hodor, hodor;
+          hodor hodor hodor? Hodor, hodor. Hodor. Hodor, hodor - HODOR hodor, hodor hodor hodor! Hodor, hodor. Hodor. Hodor, HODOR
+          hodor, hodor hodor, hodor, hodor hodor. Hodor hodor - hodor - hodor... Hodor hodor hodor hodor hodor hodor hodor?! Hodor
+          hodor - hodor hodor hodor. Hodor. Hodor hodor... Hodor hodor hodor hodor hodor? `,
+
+    thirdParagraph: `Hodor hodor - hodor... Hodor hodor hodor hodor. Hodor. Hodor! Hodor hodor, hodor hodor hodor hodor hodor; hodor hodor? Hodor!
+          Hodor hodor, HODOR hodor, hodor hodor?! Hodor! Hodor hodor, HODOR hodor, hodor hodor, hodor, hodor hodor. Hodor, hodor.
+          Hodor. Hodor, hodor, hodor. Hodor hodor... Hodor hodor hodor?! Hodor, hodor... Hodor hodor HODOR hodor, hodor hodor. Hodor.`,
+    fourthParagraph: `Hodor hodor - hodor... Hodor hodor hodor hodor. Hodor. Hodor! Hodor hodor, hodor hodor hodor hodor hodor; hodor hodor? Hodor!
+          Hodor hodor, HODOR hodor, hodor hodor?! Hodor! Hodor hodor, HODOR hodor, hodor hodor, hodor, hodor hodor. Hodor, hodor.
+          Hodor. Hodor, hodor, hodor. Hodor hodor... Hodor hodor hodor?! Hodor, hodor... Hodor hodor HODOR hodor, hodor hodor. Hodor.`
   }
+  
   
 ];
 
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -9,27 +9,81 @@ let menuItems = [
   'Log Out'
 ];
 
-/* 
+// /* 
 
-  Step 1: Write a function that will create a menu component as seen below:
+//   Step 1: Write a function that will create a menu component as seen below:
 
-  <div class="menu">
-    <ul>
-      {each menu item as a list item}
-    </ul>
-  </div>
+//   <div class="menu">
+//     <ul>
+//       {each menu item as a list item}
+//     </ul>
+//   </div>
 
-  Pass the function an array as it's only argument.
+const header = document.querySelector('.header');
+header.appendChild(newMenu(menuItems));
 
-  Step 2: Iterate over the array creating a list item <li> element for each item in the array. 
-  Add those items to the <ul>
 
-  Step 3: Using a DOM selector, select the menu button currently on the DOM.
+function newMenu(array) {
+  // Creating new elements here
+  const menu = document.createElement('div');
+  const menuList = document.createElement('ul');
+
+  // Making a new structure here
+  menu.appendChild(menuList);
+
+  // Applying stiles to elements here
+  menu.classList.add('menu');
   
-  Step 4: add a click handler to the menu button, when clicked it should toggle the class 'menu--open' on the menu itself
-
-  Step 5: return the menu component.
-
-  Step 6: add the menu component to the DOM.
+  // Creating new array for internal operations
+  let menuText = [];
   
-*/
+  for(let i = 0; i < menuItems.length; i++) {
+    // Adding new single unit li
+    let menuItem = document.createElement('li');
+    // Adding content from main array
+    menuItem.textContent = array[i];
+    // Adding classes
+    menuItem.className = 'menuItem';
+    // Moving elements to new array
+    menuText.push(menuItem);
+    // Aplying it to ul
+    menuList.appendChild(menuItem);
+  }
+  
+  console.log(menuText[0]);
+
+  // Adding click event here
+  const menuButton = document.querySelector('.menu-button');
+  
+  menuButton.addEventListener('click', event => {
+    menuList.classList.toggle('menuOpen');
+    menu.classList.toggle('menuOpen');
+    
+    menuText.forEach(item => {
+      item.classList.toggle('menuOpenitems')
+    })
+    
+  })
+
+  return menu;
+}
+
+  
+//   Pass the function an array as it's only argument.
+
+//   Step 2: Iterate over the array creating a list item <li> element for each item in the array. 
+//   Add those items to the <ul>
+
+//   Step 3: Using a DOM selector, select the menu button currently on the DOM.
+  
+//   Step 4: add a click handler to the menu button, when clicked it should toggle the class 'menu--open' on the menu itself
+
+//   Step 5: return the menu component.
+
+//   Step 6: add the menu component to the DOM.
+  
+// */
+
+
+
+
